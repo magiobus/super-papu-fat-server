4ef3eb6741f7df420410a00df2ec10c8f3a08bc1
magiobus <aalbertobus@gmail.com>
2016-07-30 18:49:43 -0500

    Header and footer jade includes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
 });
+
+router.get('/mapa', function(req, res, next) {
+  res.render('mapa', { title: 'Mapa' });
+});
+
 
 router.get('/', function(req, res, next) {
   res.render('test', {})
