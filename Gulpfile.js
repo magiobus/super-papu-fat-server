8be365eecf54eafc00af912dc7aff3abd169d4d3
magiobus <aalbertobus@gmail.com>
2016-07-30 20:42:10 -0500

    zuras code in sass version

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('sass', function () {
   return gulp.src('./public/stylesheets/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
+}, function(){
+  concatCss
 });
+
 
 gulp.task('sass:watch', function () {
   gulp.watch('./public/stylesheets/**/*.scss', ['sass']);
