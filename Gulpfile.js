4a6f7763c423057b9c7cf70ed128ea8d77f4da95
edwinvarroyo <e28v11a96@gmail.com>
2016-07-30 22:03:37 -0500

    Add map

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,11 +8,9 @@ gulp.task('sass', function () {
   return gulp.src('./public/stylesheets/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
-}, function(){
-  concatCss
 });
-
 
 gulp.task('sass:watch', function () {
   gulp.watch('./public/stylesheets/**/*.scss', ['sass']);
 });
+	
